e4c095082bc432942ac4397b7c904112c0cc7f67
chriswkingg <christopherking@rogers.com>
2020-08-02 18:39:05 -0400

    Fixed an issue where the machine would run forever due to a missing break statement.

--- VirtualMachine.c
+++ VirtualMachine.c
@@ -7,7 +7,8 @@ typedef enum {
     POP,
     ADD,
     SET,
-    HLT
+    HLT,
+    OUT
 } InstructionSet;
 
 //registers
@@ -19,15 +20,64 @@ typedef enum {
 //current program
 const int program[] = {
     PSH, 5,
-    PSH, 6,
-    ADD,
-    POP,
+    SET, A, 5,
+    ADD, 5,
+    OUT, A,
     HLT
 };
 
 int stack[256];
 int registers[NUM_REGISTERS];
 bool running = true;
+
+int fetch() {
+    return program[registers[PC]];
+}
+
+void execute(int instruction) {
+    switch(instruction) {
+        case HLT: {
+            //stops the machine
+            running = false;
+            break;
+        }
+        case PSH: {
+            //pushes next value onto the stack
+            stack[++registers[SP]] = program[++registers[PC]];
+            break;
+        }
+        case POP: {
+            //gets value and decrements sp
+            int popped_value = stack[registers[SP]--];
+            
+            //prints val
+            printf("Popped: %d \n", popped_value);
+            break;
+        }
+        case ADD: {
+            //adds number to the A register
+            int a = registers[A];
+            int b = program[++registers[PC]];
+            
+            //puts result back into the A register
+            registers[A] = a + b;
+            break;
+        }
+        case SET: {
+            //get the register to change from program
+            int reg = program[++registers[PC]];
+
+            //change the register with the next value in program
+            registers[reg] = program[++registers[PC]];
+            break;
+        }
+        case OUT: {
+            //prints out value stored in register
+            printf("Out: %d\n", registers[program[++registers[PC]]]);
+            break;  
+        }
+    }
+}
 
 int main() {
     //init SP and PC
@@ -38,47 +88,10 @@ int main() {
         //fetch 
         int current_instruction = fetch();
         registers[PC]++;
+        printf("PC: %d CI: %d \n", registers[PC], current_instruction);
         
         //decode and execute
         execute(current_instruction);
     }
     return 0;
-}
-
-int fetch() {
-    return program[registers[PC]];
-}
-
-void execute(int instruction) {
-    switch(instruction) {
-        case HLT:
-            //stops the machine
-            running = false;
-            break;
-        case PSH:
-            //pushes next value onto the stack
-            stack[++registers[SP]] = program[++registers[PC]];
-            break;
-        case POP:
-            //gets value and decrements sp
-            int popped_value = stack[registers[SP]--];
-            
-            //prints val
-            printf("Popped: %d \n", popped_value);
-            break;
-        case ADD:
-            //adds number to the A register
-            int a = registers[A];
-            int b = program[++registers[PC]];
-            
-            //puts result back into the A register
-            registers[A] = a + b;
-            break;
-        case SET:
-            //get the register to change from program
-            int reg = program[--registers[PC]];
-
-            //change the register with the next value in program
-            registers[reg] = program[--registers[PC]];
-    }
 }
