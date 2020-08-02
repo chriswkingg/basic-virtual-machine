945d0ce4f82f4d78ce7ed9c2ecc308a9bdc51cca
chriswkingg <christopherking@rogers.com>
2020-08-02 16:54:51 -0400

    Added comments and implemented basic execute stage

--- VirtualMachine.c
+++ VirtualMachine.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+//all instructions
 typedef enum {
     PSH,
     POP,
@@ -9,6 +10,7 @@ typedef enum {
     HLT
 } InstructionSet;
 
+//current program
 const int program[] = {
     PSH, 5,
     PSH, 6,
@@ -18,16 +20,50 @@ const int program[] = {
 };
 
 int program_counter = 0;
+int stack_pointer = -1;
+int stack[256];
 bool running = true;
 
 int main() {
     while(running) {
+        //fetch 
         int current_instruction = fetch();
         program_counter++;
+        
+        //decode and execute
+        execute(current_instruction);
     }
     return 0;
 }
 
 int fetch() {
     return program[program_counter];
+}
+
+void execute(int instruction) {
+    switch(instruction) {
+        case HLT:
+            //stops the machine
+            running = false;
+            break;
+        case PSH:
+            //pushes next value onto the stack
+            stack[++stack_pointer] = program[++program_counter];
+            break;
+        case POP:
+            //gets value and decrements sp
+            int popped_value = stack[stack_pointer--];
+            
+            //prints val
+            printf("Popped: %d \n", popped_value);
+            break;
+        case ADD:
+            //pops numbers to add
+            int a = stack[stack_pointer--];
+            int b = stack[stack_pointer--];
+            
+            //pushes result back onto the stack
+            stack[++stack_pointer] = a + b; 
+            break;
+    }
 }
